f759a07e116eecee04331a6e161c5fb1f9a6d839
Olavi Lintumäki <atomimalli@gmail.com>
2014-01-08 00:47:35 +0200

    Initial commit, heroku ready

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratebeer::Application.config.secret_token = '8a082a2be2b6e9d97100601d69d607d35c8896835cbb5ac3fee31b80fae9b56817222b6dfe803fefece71a8d24aee93194c57aa8655d9ab6e774bf495b2d23af'
