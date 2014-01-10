72ad822fa950c428bd66f3ca79698121b5e8258e
Olavi Lintumäki <atomimalli@gmail.com>
2014-01-11 01:21:32 +0200

    add tests for users' ratings

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,27 @@ class User < ActiveRecord::Base
   validates :username, length: { minimum: 3, maximum: 15}
   validate :password_does_not_consist_of_only_letters
 
+  def favorite_beer
+    return nil if ratings.empty?
+    ratings.sort_by{ |r| r.score }.last.beer
+  end
+
+  def group_ratings_by_style(ratings)
+    ratings.group_by{|r| r.beer.style }
+  end
+
+  def average_for_ratings_array(ratings_array)
+    #puts ratings_array
+    ratings_array.drop(1).inject(0) {|sum,rating| sum+rating.score} / ratings.count
+  end
+
+  def favorite_style
+    return nil if ratings.empty?
+    group_ratings_by_style(ratings)
+      .map{|style, ratings| [style,average_for_ratings_array(ratings)]}
+        .max{|average| average[1]}[0]
+  end
+
   def password_does_not_consist_of_only_letters
     if (password =~ /\d/).nil?
       errors.add(:password, "password must not consist of only letters")
